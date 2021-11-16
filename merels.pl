c65faf23c47d39cfec702946113d91bcacc585b7
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2021-11-16 14:24:53 +0100

    Update merels.pl
    Rows

--- merels.pl
+++ merels.pl
@@ -30,3 +30,37 @@ pair((Point, Merel), Point, Merel).
 % Merel, point and positions.
 merel_on_board((Point, Merel), Board):-
   member((Point, Merel), Board).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Rows
+row(a, b, c).
+row(c, b, a).
+row(d, e, f).
+row(f, e, d).
+row(g, h, i).
+row(i, h, g).
+row(j, k, l).
+row(l, k, j).
+row(m, n, o).
+row(o, n, m).
+row(p, q, r).
+row(r, q, p).
+row(s, t, u).
+row(u, t, s).
+row(v, w, x).
+row(x, w, v).
+row(a, j, v).
+row(v, j, a).
+row(d, k, s).
+row(s, k, d).
+row(g, l, p).
+row(p, l, g).
+row(b, e, h).
+row(h, e, b).
+row(q, t, w).
+row(w, t, q).
+row(i, m, r).
+row(r, m, i).
+row(f, n, u).
+row(u, f, f).
+row(c, o, x).
+row(x, o, c).
